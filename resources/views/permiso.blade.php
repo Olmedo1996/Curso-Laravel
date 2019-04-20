4b5bc381203bc15cd825582f530b6b3a2a1a4850
Olmedo1996 <diegoolmedog96@gmail.com>
2019-04-19 23:06:18 -0400

    trabajos con routes

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,1 @@
+<a href="{{route('permiso')}}">Ir a principal</a>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/permiso', 'PermisoController@index'); */
+//Route::get('/permiso/{nombre}', 'PermisoController@index');
+//Route::get('permiso/{nombre}/{slug?}', 'PermisoController@index');//pasar parametros
+//Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso');//con el name se puede usar el route('permiso')
+Route::get('permiso/{nombre}', function ($nombre) {
+    return $nombre;
+})->where('nombre', '[A-Za-z]+');
